c29c0668af5a40c265d5c69dce1e57153b9a07c0
gusdn00 <hwgusdn00@naver.com>
2025-05-14 15:28:49 +0900

    study

--- SSUBandHub/src/App.jsx
+++ SSUBandHub/src/App.jsx
@@ -28,8 +28,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
+}
+
+function MyButton(){
+  return(
+    <button>I'm a button</button>
+  );
 }
 
 export default App
