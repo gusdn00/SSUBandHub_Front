1b78af13a8b658d2be35532e4a889766256f0fac
gusdn00 <hwgusdn00@naver.com>
2025-05-16 14:09:12 +0900

    body

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './header'
+import TeamList from './TeamList'
 
 function App() {
-
+  var tab_array = [true,false,false,false];
   return (
     <>
       <Header />
+      <TeamList />
     </>
   )
 }
